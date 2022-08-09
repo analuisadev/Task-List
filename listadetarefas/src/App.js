2d47784a8ce9b827c073ce2f7f97389d8f181970
Ana <analuisavidal89@gmail.com>
2022-08-08 21:13:16 -0300

    Inserindo algumas coisas

--- listadetarefas/src/App.js
+++ listadetarefas/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tasks from './components/Tasks';
+import styles from './App.module.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar ReactJS',
+      completed: false,
+    },
+     
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    },
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={styles.container}>
+        <Tasks tasks={tasks}/>
+      </div>
+       
+    </>
+
+    
+    
+  )
 }
 
 export default App;
---
+++ listadetarefas/src/components/Tasks.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Tasks(props) {
+    console.log(props)
+    return (
+        <h1>Tasks</h1>
+    ) 
+}
+
+export default Tasks;
