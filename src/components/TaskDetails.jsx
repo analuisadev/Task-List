b317dc06d648d3010114d4896ba4bb4ea5dad5d8
Ana <analuisavidal89@gmail.com>
2022-08-12 10:18:58 -0300

    Inserindo Icon + Alteração da descrição das tasks

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -19,7 +19,7 @@ function TaskDetails() {
             </div>
             <div className={styles.Taskdetails_container}>
                 <h2>{params.taskTitle}</h2>
-                <p>daiowdjwoaidoawjdiwajdoiawjdaw</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam incidunt explicabo expedita quis omnis qui eligendi. Ratione architecto, quam harum suscipit excepturi consectetur? Eligendi, ipsam dolores! Voluptas odio adipisci dolorem.</p>
             </div>
         </>
     )
